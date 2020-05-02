b9c075894de37808c61af0d94dff30303eda2939
Yongzhao <wangyzh@umich.edu>
2020-05-02 13:55:12 -0400

    "add the strategy regret function for two-player games"

---
+++ open_spiel/python/algorithms/psro_v2/exploration.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class Exp3(object):
+    """
+    EXP3 algorithm for adversarial bandit.
+    """
+    def __init__(self,
+                 num_arms,
+                 gamma=0.0):
+        self.weights = np.ones(num_arms)
+        self.num_arms = num_arms
+        self.gamma = gamma
+
+    def sample(self):
+        weight_sum = np.sum(self.weights)
+        self.probability_distribution = [(1.0 - self.gamma) * (w / weight_sum) + (self.gamma / len(self.weights)) for w in self.weights]
+        self.arm_pulled = np.random.choice(range(len(self.probability_distribution)), p=self.probability_distribution)
+        return self.arm_pulled
+
+    def update_weights(self, reward):
+        rewards = np.zeros(self.num_arms)
+        rewards[self.arm_pulled] = reward/self.probability_distribution[self.arm_pulled]
+        self.weights *= np.exp(rewards * self.gamma / self.num_arms)
+
+
+class pure_exp(object):
+    def __init__(self,
+                 num_arms,
+                 gamma=0.0):
+        self.weights = np.ones(num_arms)*100
+        self.num_arms = num_arms
+        self.gamma = gamma
+
+
+    def sample(self):
+        self.probability_distribution = self.weights / np.sum(self.weights)
+        self.arm_pulled = np.random.choice(range(len(self.probability_distribution)), p=self.probability_distribution)
+        return self.arm_pulled
+
+    def update_weights(self, reward):
+        self.weights[self.arm_pulled] = (1-self.gamma) * reward + self.gamma * self.weights[self.arm_pulled]
+
--- open_spiel/python/algorithms/psro_v2/try_everything.py
+++ open_spiel/python/algorithms/psro_v2/try_everything.py
@@ -1,27 +1,6 @@
 import numpy as np
 import copy
 
-from open_spiel.python.algorithms.psro_v2.eval_utils import strategy_regret
-
-# (1) Matching Pennies
-MP_p1_meta_game = np.array([[1, -1], [-1, 1]])
-MP_p2_meta_game = np.array([[-1, 1], [1, -1]])
-MP_meta_games = [MP_p1_meta_game, MP_p2_meta_game]
-
-#(2) Battle of Sexes
-BOS_p1_meta_game = np.array([[3, 0], [0, 2]])
-BOS_p2_meta_game = np.array([[2, 0], [0, 3]])
-BOS_meta_games = [BOS_p1_meta_game, BOS_p2_meta_game]
-
-#(3) Bar Crowding Game (3 players)
-BC_p1_meta_game = np.array([[[-1, 2],[1, 1]], [[2, 0], [1, 1]]])
-BC_p2_meta_game = np.array([[[-1, 1],[2, 1]], [[2, 1], [0, 1]]])
-BC_p3_meta_game = np.array([[[-1, 2],[2, 0]], [[1, 1], [1, 1]]])
-BC_meta_games = [BC_p1_meta_game, BC_p2_meta_game, BC_p3_meta_game]
-
-ne, dev, regret = strategy_regret(BOS_meta_games)
-
-
-print(ne)
-print(dev)
-print(regret)
+a = np.array([1,2,3])
+print(a*np.exp(np.array([2,3,4])))
+print(np.exp(2), 2*np.exp(3))
