4eca1c78f58013d925e03ec6a8053d97ee9371ae
Yongzhao <wangyzh@umich.edu>
2020-05-05 00:30:44 -0400

    "gambit oops fixed."

--- open_spiel/python/algorithms/psro_v2/try_everything.py
+++ open_spiel/python/algorithms/psro_v2/try_everything.py
@@ -1,6 +1,7 @@
 import numpy as np
 import copy
 from open_spiel.python.algorithms.psro_v2.eval_utils import regret, strategy_regret
+from open_spiel.python.algorithms.nash_solver.general_nash_solver import nash_solver
 
 
 BOS_p1_meta_game = np.array([[-1, 0], [0, 5]])
@@ -8,3 +9,4 @@ BOS_p2_meta_game = np.array([[-1, 0], [0, 5]])
 BOS_meta_games = [BOS_p1_meta_game, BOS_p2_meta_game]
 
 print(strategy_regret(BOS_meta_games, 1))
+
