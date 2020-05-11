591048a97eb1489fbce3a452cdc833822fdd9957
yongzhao <wangyzh@umich.edu>
2020-05-11 19:14:00 -0400

    "fix br set/get weights"

--- open_spiel/python/algorithms/psro_v2/meta_strategies.py
+++ open_spiel/python/algorithms/psro_v2/meta_strategies.py
@@ -255,7 +255,7 @@ def weighted_NE_strategy(solver, return_joint=False, checkpoint_dir=None, gamma=
   meta_games = solver.get_meta_game()
   num_players = len(meta_games)
   NE_list = solver._NE_list
-  print("NE_list:", NE_list)
+  print("NE_list:", NE_list, num_players)
   if len(NE_list) == 0:
     return [np.array([1.])] * num_players
 
