2fe245253a3c2abdadb2941191d4320c41d5f2cb
qmaai <qmaai@connect.ust.hk>
2020-05-17 00:43:54 -0400

    use combined game to evaluate NEs from all runs, evaluate regrets curve for all runs, regret function, players could have different number of strategies

--- open_spiel/python/algorithms/psro_v2/combined_game.py
+++ open_spiel/python/algorithms/psro_v2/combined_game.py
@@ -1,3 +1,26 @@
+"""
+The script calculate combined game and evaluates ne performance of each run or evaluate iteration regrets of every run. Assumes a standard file structure. Please copy all relevant experiment runs into a root folder and supply the root folder through FLAGS.
+--root_result_folder
+    --experiment1
+        |--meta_game.pkl
+        |--nash_prob
+        |  |--1.pkl
+        |  |--2.pkl
+        |  |--...
+        |--strategies
+        |  |--kwarg.pkl
+        |  |--env.pkl(env args here for policy initialization)
+        |  |--player_0
+        |  |  |--1.pkl
+        |  |  |--2.pkl
+        |  |  |--...
+        |  |--player_1
+        |  |--...
+
+    --experiment2
+    ...
+"""
+
 import pyspiel
 from open_spiel.python import rl_environment
 from open_spiel.python.algorithms.psro_v2.utils import set_seed
@@ -8,12 +31,19 @@ from absl import app
 from absl import flags
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer("seed", None, "seed")
+# calculate combined game only with ne strategies
 flags.DEFINE_bool("only_combine_nash", False, "only combine nash in combined game")
+flags.DEFINE_bool("evaluate_nash",True, "evaluate regret of final nash for all runs against meta_game")
+flags.DEFINE_bool("evaluate_iters",False, "evaluate regret curve for all runs against meta_game")
+
+flags.DEFINE_integer("seed", None, "seed")
 flags.DEFINE_string("root_result_folder","","root result folder that contains the experiments runs to be calculated into combined game")
+flags.DEFINE_string("combine_game_path","","provide combined game pkl if there already is an existing comnbined game, in this case, only need to calculate exploitabilities of each run")
 flags.DEFINE_integer("sims_per_entry", 1000,
                      ("Number of simulations to run to estimate each element"
                       "of the game outcome matrix."))
+
+ZERO_THRESHOLD = 0.001
 
 def calculate_combined_game(checkpoint_dirs,
                             combined_game_save_path,
@@ -86,12 +116,12 @@ def calculate_combined_game(checkpoint_dirs,
                 nash_root_dir = checkpoint_dirs[i] + "/nash_prob"
                 final_nash_file = os.path.join(nash_root_dir, str(num_strategy-1)+".pkl")
                 nash_ne = load_pkl(final_nash_file)
-                none_zero_index = [[i for i in range(len(ele)) if ele[i]>0.001] \
+                none_zero_index = [[i for i in range(len(ele)) if ele[i] > ZERO_THRESHOLD] \
                     for ele in nash_ne]
                 meta_games[i] = [ele[np.ix_(*none_zero_index)] for ele in meta_games[i]]
 
             for weight_file in range(1,num_strategy+1): # traverse the weight file
-                if only_combine_nash and nash_ne[p][weight_file-1] < 0.001:
+                if only_combine_nash and nash_ne[p][weight_file-1] < ZERO_THRESHOLD:
                     continue # only combine nash strategies
                 weight = load_pkl(os.path.join(strat_player_dir,str(weight_file)+'.pkl'))
                 strategy = load_strategy(strategy_type, strategy_kwargs, env, p, weight)
@@ -127,62 +157,178 @@ def calculate_combined_game(checkpoint_dirs,
             for p in range(num_player):
                 combined_game[p][index] = rewards[p]
         _ = it.iternext()
+    
+    save_path =  os.path.join(combined_game_save_path,"combined_game_se_"+str(seed)+'_neonly_'+str(only_combine_nash)+".pkl")
+    save_pkl(combined_game, save_path)
+    print("combined game {} only combine NE saved at {}".format(only_combine_nash,save_path))
 
-    save_pkl(combined_game, os.path.join(combined_game_save_path,"combined_game_se_"+str(seed)+'_neonly_'+str(only_combine_nash)+".pkl"))
+    return combined_game, strategy_num
 
-    return combined_game 
 
-def calculate_regret_from_combined_game(ne_dir, strat_start, strat_end, combined_game_path):
+def calculate_regret_from_combined_game(ne_dir,
+                                        combined_game
+                                        strat_start,
+                                        strat_end,
+                                        checkpoint_dir):
     """
-    WARNING: assumes automatically that all strategies represneted by ne_dir
-             is in the combined_game, ranged from strat_start and consecutive. This is 99% likely not the case 
-    Calculate the exploitability of each iteration, the number of iterations equaling
-    to the number of files in the ne_dir.
+    Calculate the exploitability of all possible iterations
+    The number of max iterations evaluatable depends on strat_start and strat_end
+    Assume that each player has the same number of strategies in subgame
     Params:
-        ne_dir             : directory that contains and only contains int.pkl, absolute path
-        combined_game_path : the file that contains the last combined game
+        ne_dir         : directory that contains and only contains int.pkl, absolute path
+        strat_start    : starting index of this subgame in combined game
+        strat_end      : ending index of this subgame in combined game
+        checkpoint_dir : directory to save the exploitability curve
     Returns:
-        save exp file to exp.csv at the same directory level with combined_game_path
+        save exp file to exp.csv under checkpoint_dir
         return exp
     """
-    combined_game = load_pkl(combined_game_path)
     exploitability = [[],[]]
+    # ne.pkl saving iteration ahead of meta_games.pkl
+    # so not all ne.pkl's strategies are in the meta_games,
+    # consequently not in combined_game
+    max_gpsro_iter = strat_end - strat_start - 1
     for fi in os.listdir(ne_dir):
-        subgame_index = int(fi.split('.pkl')[0])+1+strat_start
-        file_name = os.path.join(ne_dir, fi)
-        eq = load_pkl(file_name)
+        gpsro_iter = int(fi.split('.pkl')[0])
+        if gpsro_iter > gpsro_iter:
+            continue        
+        subgame_index = gpsro_iter + 1 + strat_start
+        eq = load_pkl(os.path.join(ne_dir, fi))
         # calculate regret
         regrets = regret(combined_game, subgame_index, subgame_ne=eq, start_index=strat_start)
         exploitability[0].append(sum(regrets))
         exploitability[1].append(subgame_index)
     
-    # write the exploitabilities to csv file
-    result_file = '/'.join(combined_game_path.split('/')[:-1]) + 'exp.csv'
+    # exploitability[1] carries gpsro_iteration
+    # sort exploitibility according to iteration
     exploitability = [x for _, x in sorted(zip(exploitability[1],exploitability[0]))]
+
+    result_file = os.path.join(checkpoint_dir, 'exp.csv')
     df = pd.DataFrame(exploitability)
     df.to_csv(result_file,index=False)
-    
+    print("NashConv saved to", result_file) 
+
     return exploitability
+
+
+def calculate_iterations_regret_over_meta_game(checkpoint_dirs, combined_game):
+    """
+    Go over all experiments and calculate regret curves for each run
+    assumes that combined game include all strategies inside each run
+    """
+    num_player = len(combined_game)
+    regret_curves = []
+    current_index = 0
+    for di in checkpoint_dirs:
+        snum = check_strategies_num_in_combined_game(di, True)
+        ne_dir = os.path.join(di,"nash_prob")
+        regret_curve = calculate_regret_from_combined_game(ne_dir,
+                                                           combined_game,
+                                                           current_index,
+                                                           current_index + snum,
+                                                           di)
+        current_index += snum
+        regret_curves.append(regret_curve)
+
+    return regret_curves
+
+def check_strategies_num_in_combined_game(checkpoint_dir,
+                                          only_return_strategy_length=False):
+    """
+    From meta game length, decide gpsro_iteration. Then decide which ne.pkl to use,
+    and eventually decide how many strategies of subgame are in the meta_game
+    Return:
+        strats_num   : number of strategies. A list of len num_player
+        strats_num_ne: number of ne strategies
+        strats_ne    : list of list, index of ne support strategies for each player
+    """
+    meta_game = load_pkl(os.path.join(checkpoint_dir, "meta_game.pkl"))
+    gpsro_it = meta_game[0].shape[0][0]-1 # -1 because of the initial policy
+    strats_num = np.ones(len(meta_game))*(gpsro_it+1)
+    if only_return_strategy_length:
+        return strats_num
+
+    ne_folder = os.path.join(checkpoint_dir, "nash_prob")
+    ne = load_pkl(os.path.join(ne_folder,str(gpsro_it)+".pkl"))
+    strats_ne = [[i for i in range(len(ele)) if ele[i] > ZERO_THRESHOLD] for ele in ne]
+    strats_num_ne = np.array([len(ele) for ele in strats])
+
+    return strats_num, strats_num_ne, strats_ne, ne
+    
+
+def calculate_final_ne_performance_over_meta_game(checkpoint_dirs,
+                                                  combined_game,
+                                                  combined_game_only_contains_ne=True):
+    """
+    Evaluate regret of nash equilibrium in each experiment file, against combined game
+    Tricky part is combined game could contain only ne/ all strategies
+    Params:
+        checkpoint_dirs : a list of checkpoint dir, one for each experiment
+        combined_game   : list of list, each one for one player
+        combined_game_only_contains_ne: combined game only contains nash equilibrium strategies of experiment runs or not
+    """
+    num_player = len(combined_game)
+    current_index = np.zeros(num_player)
+    exploitabilities = []
+    for di in checkpoint_dirs:
+        snum, snum_ne, stra_ne, ne = check_strategies_num_in_combined_game(di)
+        if combined_game_only_contains_ne:
+            ne = [np.array([x for x in ele if x > ZERO_THRESHOLD]) for ele in ne]
+            subgame_index = current_index + snum_ne
+        else:
+            subgame_index = current_index + snum
+        exploitabilities.append(regret(combined_game,
+                                       subgame_index,
+                                       subgame_ne=ne,
+                                       start_index=current_index))
+        current_index += snum_ne if combined_game_only_contains_ne else snum
+
+    return np.array(exploitabilities).sum(axis=1)
+
 
 def main(argv):
     if len(argv) > 1:
       raise app.UsageError("Too many command-line arguments.")
   
     assert os.path.isdir(FLAGS.root_result_folder), "no such directory"
-    dir_content = os.listdir(FLAGS.root_result_folder)
-    assert len(dir_content)>1, "directory provided cannot make combined game"
+
     checkpoint_dirs = [os.path.join(FLAGS.root_result_folder, x) \
         for x in os.listdir(FLAGS.root_result_folder) \
         if os.path.isdir(os.path.join(FLAGS.root_result_folder, x))]
-  
-    seed = FLAGS.seed if FLAGS.seed else np.random.randint(low=0, high=1e5)
-    set_seed(seed)
+    
+        seed = FLAGS.seed if FLAGS.seed else np.random.randint(low=0, high=1e5)
+        set_seed(seed)
 
-    combined_game = calculate_combined_game(checkpoint_dirs,
-                                            FLAGS.root_result_folder,
-                                            only_combine_nash=FLAGS.only_combine_nash,
-                                            sims_per_entry=FLAGS.sims_per_entry,
-                                            seed=seed)
 
+    # combined game supplied, no need to calculate again
+    if FLAGS.combined_game_path == "":
+        dir_content = os.listdir(FLAGS.root_result_folder)
+        assert len(dir_content)>1, "directory provided cannot make combined game"
+             
+        # calculate combined game
+        combined_game = calculate_combined_game(checkpoint_dirs,
+                                                FLAGS.root_result_folder,
+                                                only_combine_nash=FLAGS.only_combine_nash,
+                                                sims_per_entry=FLAGS.sims_per_entry,
+                                                seed=seed)
+        combined_game_only_contains_ne = FLAGS.only_combine_nash
+    else:
+        combined_game = load_pkl(FLAGS.combined_game_path)
+        combined_game_only_contains_ne = '_neonly_True' in FLAGS.combined_game_path
+    
+    if FLAGS.evaluate_nash:
+        
+        exp = calculate_final_ne_performance_over_meta_game(checkpoint_dirs,\
+            combined_game,\
+            combined_game_only_contains_ne=combined_game_only_contains_ne)
+        
+        save_pkl(exp,os.path.join(FLAGS.root_result_folder,\
+            "nash_exp_neonly_"+combined_game_only_contains_ne+".pkl"))
+    
+    if FLAGS.evaluate_iters:
+        assert not combined_game_only_contains_ne,\
+            "combined game doesn't contain all strategies"
+        calculate_iterations_regret_over_meta_game(checkpoint_dirs, combined_game)
+    
 if __name__ == "__main__":
     app.run(main)
--- open_spiel/python/algorithms/psro_v2/eval_utils.py
+++ open_spiel/python/algorithms/psro_v2/eval_utils.py
@@ -11,27 +11,29 @@ import pickle
 import os
 import tensorflow.compat.v1 as tf
 
-#def regret(meta_games, subgame_index, subgame_ne=None):
+#def regret(meta_games, subgame_index, subgame_ne=None, start_index=0):
 #    """
 #    Calculate the regret based on a complete payoff matrix for PSRO
 #    Assume all players have the same number of policies
 #    :param meta_games: meta_games in PSRO
-#    :param subgame_index: the subgame to evaluate. Redundant when subgame_ne is supplied
+#    :param subgame_index: the subgame to evaluate. Redundant when subgame_ne is supplied. The index of last policy in subgame
+#    :param start_index: starting index for the subgame
 #    :param: subgame_ne: subgame nash equilibrium vector.
 #    :return: a list of regret, one for each player.
 #    """
 #    num_policy = np.shape(meta_games[0])[0]
 #    num_players = len(meta_games)
-#    if num_policy == subgame_index:
+#    if num_policy == subgame_index-start_index:
 #        print("The subgame is same as the full game. Return zero regret.")
 #        return np.zeros(num_players)
-#    num_new_pol = num_policy - subgame_index
 #
-#    index = [list(np.arange(subgame_index)) for _ in range(num_players)]
+#    num_new_pol_back = num_policy - subgame_index
+#
+#    index = [list(np.arange(start_index,subgame_index)) for _ in range(num_players)]
 #    submeta_games = [ele[np.ix_(*index)] for ele in meta_games]
 #    nash = nash_solver(submeta_games, solver="gambit") if not subgame_ne else subgame_ne
 #    prob_matrix = meta_strategies.general_get_joint_strategy_from_marginals(nash)
-#    this_meta_prob = [np.append(nash[i],[0 for _ in range(num_new_pol)]) for i in range(num_players)]
+#    this_meta_prob = [np.concatenate([0 for _ in range(start_index)], nash[i], [0 for _ in range(num_new_pol_back)]) for i in range(num_players)]
 #
 #    nash_payoffs = []
 #    deviation_payoffs = []
@@ -40,10 +42,13 @@ import tensorflow.compat.v1 as tf
 #        ne_payoff = np.sum(submeta_games[i]*prob_matrix)
 #        # iterate through player's new policy
 #        dev_payoff = []
-#        for j in range(num_new_pol):
+#        for j in range(start_index + num_new_pol_back):
 #            dev_prob = this_meta_prob.copy()
 #            dev_prob[i] = np.zeros(num_policy)
-#            dev_prob[i][subgame_index+j] = 1
+#            if j < start_index:
+#                dev_prob[i][j] = 1
+#            else:
+#                dev_prob[i][subgame_index+j] = 1
 #            new_prob_matrix = meta_strategies.general_get_joint_strategy_from_marginals(dev_prob)
 #            dev_payoff.append(np.sum(meta_games[i]*new_prob_matrix))
 #        deviation_payoffs.append(dev_payoff-ne_payoff)
@@ -55,26 +60,33 @@ import tensorflow.compat.v1 as tf
 def regret(meta_games, subgame_index, subgame_ne=None, start_index=0):
     """
     Calculate the regret based on a complete payoff matrix for PSRO
-    Assume all players have the same number of policies
+    In subgame, each player could have different number of strategies
     :param meta_games: meta_games in PSRO
-    :param subgame_index: the subgame to evaluate. Redundant when subgame_ne is supplied
-    :param start_index: starting index for the subgame
+    :param subgame_index: last policy index in subgame.
+                          int/list. If int, players have same num of strategies
+    :param start_index: starting index for the subgame.
+                          int/list. If int, assume subgame in all num_players dimension
+                          have the same index
     :param: subgame_ne: subgame nash equilibrium vector.
     :return: a list of regret, one for each player.
     """
-    num_policy = np.shape(meta_games[0])[0]
+    num_policy = np.array(np.shape(meta_games[0]))
     num_players = len(meta_games)
-    if num_policy == subgame_index-start_index:
+    subgame_index = np.ones(num_players,dtype=int)*subgame_index \
+        if isinstance(subgame_index,int) else subgame_index
+    start_index = np.ones(num_player,dtype=int)*start_index \
+        if isinstance(start_index,int) else start_index
+    if not sum(num_policy != subgame_index-start_index):
         print("The subgame is same as the full game. Return zero regret.")
         return np.zeros(num_players)
 
     num_new_pol_back = num_policy - subgame_index
 
-    index = [list(np.arange(start_index,subgame_index)) for _ in range(num_players)]
+    index = [list(np.arange(start_index[i],subgame_index[i])) for i in range(num_players)]
     submeta_games = [ele[np.ix_(*index)] for ele in meta_games]
     nash = nash_solver(submeta_games, solver="gambit") if not subgame_ne else subgame_ne
     prob_matrix = meta_strategies.general_get_joint_strategy_from_marginals(nash)
-    this_meta_prob = [np.concatenate([0 for _ in range(start_index)], nash[i], [0 for _ in range(num_new_pol_back)]) for i in range(num_players)]
+    this_meta_prob = [np.concatenate([0 for _ in range(start_index[i])], nash[i], [0 for _ in range(num_new_pol_back[i])]) for i in range(num_players)]
 
     nash_payoffs = []
     deviation_payoffs = []
@@ -83,13 +95,13 @@ def regret(meta_games, subgame_index, subgame_ne=None, start_index=0):
         ne_payoff = np.sum(submeta_games[i]*prob_matrix)
         # iterate through player's new policy
         dev_payoff = []
-        for j in range(start_index + num_new_pol_back):
+        for j in range(start_index[i] + num_new_pol_back[i]):
             dev_prob = this_meta_prob.copy()
-            dev_prob[i] = np.zeros(num_policy)
-            if j < start_index:
+            dev_prob[i] = np.zeros(num_policy[i])
+            if j < start_index[i]:
                 dev_prob[i][j] = 1
             else:
-                dev_prob[i][subgame_index+j] = 1
+                dev_prob[i][subgame_index[i]+j] = 1
             new_prob_matrix = meta_strategies.general_get_joint_strategy_from_marginals(dev_prob)
             dev_payoff.append(np.sum(meta_games[i]*new_prob_matrix))
         deviation_payoffs.append(dev_payoff-ne_payoff)
@@ -184,119 +196,6 @@ def rollout(env, strategies, strategy_support, sims_per_entry=1000):
 
     return [np.sum(payoff_tensor[i]*prob_matrix) for i in range(num_players)]
 
-def calculate_combined_game(strat_and_meta_game_li, combined_game_save_path, sims_per_entry=1000, env_file_name="env.pkl", strategy_kwarg_file_name="kwarg.pkl", meta_game_file_name="meta_game.pkl"):
-    """
-    Calculate combined game
-    Params:
-        strat_and_meta_game_li : a list of checkpoint directory 
-        combined_game_save_path: a parent directory where calculated combined game is to be saved
-    Assumes directory structure to strategies are
-    --meta_game.pkl (meta_game_file_name)
-    --strategies (params given)
-        --kwarg.pkl (strategy_kwarg_file_name)
-        --env.pkl   (env_file_name)
-        --player_0
-            1.pkl
-            2.pkl
-            ...
-        --player_1
-        ...
-    """
-    num_runs = len(strat_and_meta_game_li)
-
-    # process strategy dir. 
-    strategies = [] # a list of length num_runs. Each sub list contains number of player sublist
-    meta_games = []
-    for i in range(num_runs): # traverse all runs
-        meta_games.append(load_pkl(os.path.join(strat_and_meta_game_li[i],meta_game_file_name)))
-        strat_root_dir = strat_and_meta_game_li[i] + "/strategies"
-        player_dir = [x for x in os.listdir(strat_root_dir) if os.path.isdir(os.path.join(strat_root_dir,x))]
-
-        # initialize game once
-        if i == 0:
-            env_kwargs = load_pkl(os.path.join(strat_root_dir, env_file_name))
-            game = pyspiel.load_game_as_turn_based(env_kwargs["game_name"], env_kwargs["param"])
-        # load strategy kwargs: each run might have different strategy type and kwargs
-        strategy_kwargs = load_pkl(os.path.join(strat_root_dir, strategy_kwarg_file_name))
-        strategy_type = strategy_kwargs["policy_class"]
-        strategy_kwargs = strategy_kwargs["strategy_kwargs"]
-
-        strategy_run = []
-        for p in len(player_dir):  # traverse players from each run
-            strategy_player = []
-            strat_player_dir = os.path.join(strat_root_dir,player_dir[p])
-            num_strategy = len(os.listdir(strat_player_dir))
-            for weight_file in range(num_strategy)+1: # traverse the weight file
-                weight = load_pkl(os.path.join(strat_player_dir,str(weight_file)+'.pkl'))
-                strategy = load_strategy(strategy_type, strategy_kwargs, p, weight)
-                strategy_player.append(strategy)
-            strategy_run.append(strategy_player)
-        strategies.append(strategy_run)
-    
-    # build the meta_game matrix
-    num_player = len(strategies[0])
-    # strategy_num: np array of size: num_runs* num_player
-    strategy_num = np.array([[len(ele[i]) for i in range(num_player)] for ele in strategies])
-    combined_game = [np.zeros(list(strategy_num.sum(axis=0)))*np.nan for _ in range(num_player)]
-    
-    # fill in the existing meta_game values
-    for runs in range(num_runs):
-        meta_game_start_index = strategy_num[:runs].sum(axis=0)
-        meta_game_end_index = strategy_num[:runs+1].sum(axis=0)
-        index = [slice(meta_game_start_index[j],meta_game_end_index[j],1) \
-            for j in range(num_player)]
-        for p in range(num_player):
-            combined_game[index] = meta_games[i]
-    
-    # reshape strategies in terms of combined game
-    strategies_combined = [list(itertools.chain.from_iterable(ele[p] for ele in strategies)) \
-        for p in range(num_player)]
-    # sample the missing entries
-    it = np.nditer(combined_game[0],flags=['multi_index'])
-    while not it.finished:
-        index = it.multi_index
-        if not np.isnan(combined_game[0][index]):
-            agents = [strategies_combined[p][index[p]] for p in range(num_player)]
-            rewards = sample_episode(env, agents, sims_per_entry)
-            for p in range(num_player):
-                combined_game[p][index] = rewards[p]
-        _ = it.iternext()
-
-    save_pkl(combined_game, os.path.join(combined_game_save_path,"combined_game.pkl"))
-
-    return combined_game 
-
-def calculate_regret_from_combined_game(ne_dir, strat_start, combined_game_path):
-    """
-    WARNING: assumes automatically that all strategies represneted by ne_dir
-             is in the combined_game, ranged from strat_start and consecutive. 
-    Calculate the exploitability of each iteration, the number of iterations equaling
-    to the number of files in the ne_dir.
-    Params:
-        ne_dir             : directory that contains and only contains int.pkl, absolute path
-        combined_game_path : the file that contains the last combined game
-    Returns:
-        save exp file to exp.csv at the same directory level with combined_game_path
-        return exp
-    """
-    combined_game = load_pkl(combined_game_path)
-    exploitability = [[],[]]
-    for fi in os.listdir(ne_dir):
-        subgame_index = int(fi.split('.pkl')[0])+1+strat_start
-        file_name = os.path.join(ne_dir, fi)
-        eq = load_pkl(file_name)
-        # calculate regret
-        regrets = regret(combined_game, subgame_index, subgame_ne=eq, start_index=strat_start)
-        exploitability[0].append(sum(regrets))
-        exploitability[1].append(subgame_index)
-    
-    # write the exploitabilities to csv file
-    result_file = '/'.join(combined_game_path.split('/')[:-1]) + 'exp.csv'
-    exploitability = [x for _, x in sorted(zip(exploitability[1],exploitability[0]))]
-    df = pd.DataFrame(exploitability)
-    df.to_csv(result_file,index=False)
-    
-    return exploitability
 
 class SElogs(object):
     def __init__(self,
