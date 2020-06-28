ec76606fa80517505dbe78c4354751d5cf65775f
wangyzh <wangyzh@umich.edu>
2020-06-27 21:07:53 -0400

    "update slurm10_prd_rect.sh"

--- open_spiel/python/algorithms/psro_v2/slurm11_DO_rect.sh
+++ open_spiel/python/algorithms/psro_v2/slurm11_DO_rect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=dqn_fic
+#SBATCH --job-name=dqn_do_rect
 #SBATCH --mail-user=wangyzhsrg@aol.com
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
@@ -12,5 +12,5 @@
 
 module load python3.6-anaconda/5.2.0
 cd ${SLURM_SUBMIT_DIR}
-python psro_v2_example.py --game_name=leduc_poker --n_players=2 --meta_strategy_method=uniform --oracle_type=DQN --gpsro_iterations=150 --number_training_episodes=10000 --sbatch_run=True --root_result_folder=dqn_fic
+python psro_v2_example.py --game_name=leduc_poker --n_players=2 --meta_strategy_method=nash --oracle_type=DQN --rectifier=rectified --gpsro_iterations=150 --number_training_episodes=10000 --sbatch_run=True --root_result_folder=dqn_do_rect
 
