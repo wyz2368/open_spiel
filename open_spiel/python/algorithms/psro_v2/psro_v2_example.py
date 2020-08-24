7c2066183ec32146a7517b1cbb1cc766522988c7
wangyzh <wangyzh@umich.edu>
2020-08-23 22:38:53 -0400

    "update slurm from DQN to BR"

--- open_spiel/python/algorithms/psro_v2/psro_v2_example.py
+++ open_spiel/python/algorithms/psro_v2/psro_v2_example.py
@@ -422,8 +422,8 @@ def gpsro_looper(env, oracle, agents, writer, quiesce=False, checkpoint_dir=None
     train_reward_curve = g_psro_solver.iteration(seed=seed)
     meta_game = g_psro_solver.get_meta_game()
     meta_probabilities = g_psro_solver.get_meta_strategies()
-    # nash_meta_probabilities = g_psro_solver.get_nash_strategies()
-    nash_meta_probabilities = g_psro_solver.get_prd_strategies()
+    nash_meta_probabilities = g_psro_solver.get_nash_strategies()
+    # nash_meta_probabilities = g_psro_solver.get_prd_strategies()
     policies = g_psro_solver.get_policies()
    
     if FLAGS.verbose:
@@ -454,7 +454,7 @@ def gpsro_looper(env, oracle, agents, writer, quiesce=False, checkpoint_dir=None
 
     if gpsro_iteration % 10 ==0:
       save_at_termination(solver=g_psro_solver, file_for_meta_game=checkpoint_dir+'/meta_game.pkl')
-      save_strategies(solver=g_psro_solver, checkpoint_dir=checkpoint_dir)
+      # save_strategies(solver=g_psro_solver, checkpoint_dir=checkpoint_dir)
     
     beneficial_deviation = print_beneficial_deviation_analysis(last_meta_game, meta_game, last_meta_prob, FLAGS.verbose)
     last_meta_prob, last_meta_game = meta_probabilities, meta_game
