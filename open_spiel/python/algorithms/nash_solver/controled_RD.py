8d09add6e4dfe4a4cce0b1ac68b28ffd04167eaa
wangyzh <wangyzh@umich.edu>
2021-01-24 14:58:26 -0500

    "update true prd_iteration"

--- open_spiel/python/algorithms/nash_solver/controled_RD.py
+++ open_spiel/python/algorithms/nash_solver/controled_RD.py
@@ -14,7 +14,7 @@ from open_spiel.python.algorithms.psro_v2.eval_utils import dev_regret
 def controled_replicator_dynamics(payoff_tensors,
                                   regret_threshold,
                                   prd_initial_strategies=None,
-                                  prd_iterations=int(5e6),
+                                  prd_iterations=int(1e5),
                                   prd_dt=1e-3,
                                   prd_gamma=0,
                                   average_over_last_n_strategies=None,
