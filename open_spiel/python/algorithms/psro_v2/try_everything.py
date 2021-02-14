0bd97fa8e0ff9cd8a6614d0eb1bdec003bb6a225
wangyzh <wangyzh@umich.edu>
2021-02-14 17:10:03 -0500

    "debug [0,0,0]"

--- open_spiel/python/algorithms/psro_v2/try_everything.py
+++ open_spiel/python/algorithms/psro_v2/try_everything.py
@@ -4,6 +4,6 @@ import os
 import itertools
 # from  open_spiel.python.algorithms.psro_v2.eval_utils import regret, strategy_regret
 
-eq = [np.array([0.2, 0.8]), np.array([0.1, 0.9])]
-ne_subgame_nonzero = [ele[ele >= 0.5] for ele in eq]
-print(ne_subgame_nonzero)
+a = [[0,0,0,1,1], [0,0,0,1,1]]
+cum_sum = [np.cumsum(ele) for ele in a]
+print(cum_sum)
