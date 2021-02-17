b5d2a8dd1597f72e9c74b80a2bd293448af930a3
wangyzh <wangyzh@umich.edu>
2021-02-16 22:29:11 -0500

    "update slurm1_quiesce.sh"

--- open_spiel/python/algorithms/psro_v2/slurm1_quiesce.sh
+++ open_spiel/python/algorithms/psro_v2/slurm1_quiesce.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=quiesce
+#SBATCH --job-name=quiesce_d
 #SBATCH --mail-user=wangyzhsrg@aol.com
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=14g
+#SBATCH --mem-per-cpu=40g
 #SBATCH --time=10-00:00:00
 #SBATCH --account=wellman1
-#SBATCH --partition=standard
+#SBATCH --partition=largemem
 
 module load python3.6-anaconda/5.2.0
 cd ${SLURM_SUBMIT_DIR}
