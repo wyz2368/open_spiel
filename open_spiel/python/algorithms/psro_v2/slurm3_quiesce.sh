67aee240016907d9447edf3f8d60345041de1398
wangyzh <wangyzh@umich.edu>
2021-03-12 21:15:57 -0500

    "update slurm3_quiesce.sh"

---
+++ open_spiel/python/algorithms/psro_v2/slurm3_quiesce.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=quiesce_d
+#SBATCH --mail-user=wangyzhsrg@aol.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=20g
+#SBATCH --time=13-00:00:00
+#SBATCH --account=wellman1
+#SBATCH --partition=standard
+
+module load python3.6-anaconda/5.2.0
+cd ${SLURM_SUBMIT_DIR}
+python psro_v2_quiesce.py --quiesce_regret_threshold=0.0 --RD_regret_threshold=0.5 --RD_regularization=True --quiesce_control=False --game_name=leduc_poker --n_players=3 --meta_strategy_method=prd --oracle_type=DQN --gpsro_iterations=50 --number_training_episodes=10000 --sbatch_run=True --root_result_folder=dqn_RD_crd_quiesce_050
+
